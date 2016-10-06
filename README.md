4e32a40919d7d0f32c96fca512d9bdaf92e981e7
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-10-06 11:16:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# eslint-config-netlify
+
+ESLint rules for Netlify JavaScript projects
+
+## Installation
+
+1. `npm install --save-dev eslint eslint-config-netlify`
+2. Add to `package.json`
+
+```json
+"eslintConfig": {
+    "extends": [
+      "eslint-config-netlify"
+    ]
+  }
+```
+
+or to `.eslintrc`
+
+```json
+{
+  "extends": [
+    "eslint-config-netlify"
+  ]
+}
+```
---
+++ index.js
@@ -0,0 +1,46 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    commonjs: true,
+    mocha: true,
+    jest: true,
+  },
+  extends: [
+    'eslint-config-airbnb',
+  ],
+  plugins: [
+    'class-property',
+  ],
+  globals: {
+    netlify: true,
+    CMS_ENV: true,
+  },
+  rules: {
+    // Overrides from the AirBnb configs
+    'no-console': 'error',
+    'template-curly-spacing': ['error', 'always'],
+    'jsx-quotes': ['error', 'prefer-double'],
+    'max-len': ['error', 160, 2, {
+      ignoreUrls: true,
+      ignoreComments: false,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+    }],
+
+    // React
+    'react/jsx-handler-names': ['error', {
+      eventHandlerPrefix: 'handle',
+      eventHandlerPropPrefix: 'on',
+    }],
+    'react/jsx-key': 'error',
+    'react/jsx-max-props-per-line': ['error', { maximum: 2 }],
+    'react/no-direct-mutation-state': 'error',
+    'react/jsx-filename-extension': ['off', { extensions: ['.jsx'] }],
+
+    // Class properties
+    'class-property/class-property-semicolon': 2,
+  },
+};
