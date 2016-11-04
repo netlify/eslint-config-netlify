9b113a295b00b943ba067f22485cb16fb857cccd
Brian Douglas <bdougie@users.noreply.github.com>
2016-11-04 09:23:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -28,8 +28,7 @@ module.exports = {
       allow: ['resolve', 'reject', 'done', 'cb'],
     }],
     'template-curly-spacing': [2, 'always'],
-    "quotes": [2, "double", "avoid-escape"],
-    'jsx-quotes': [2, 'prefer-double'],
+    'jsx-quotes': [2, 'prefer-single'],
     'max-len': [2, {
       code: 100,
       comments: 100,
