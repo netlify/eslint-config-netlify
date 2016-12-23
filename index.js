e9b03f8fe620baf7ee41a584ad5ca345be4e1a08
Cássio Zen <cassiozen@gmail.com>
2016-12-23 14:45:18 -0200

    More fine tuning: ref eslint-config-airbnb-yhc

--- index.js
+++ index.js
@@ -23,21 +23,27 @@ module.exports = {
   rules: {
     // Overrides
     "quotes": 0,
+    "no-plusplus": 0,
+    "no-param-reassign": 0,
+    "no-trailing-spaces": 0,
+    "space-infix-ops": 0,
+    "key-spacing": [0, {beforeColon: false, afterColon: true, mode: "minimum",}],
     "max-len": [1, 120, 2, {ignoreComments: true}],
     "quote-props": [1, "consistent-as-needed"],
     "no-cond-assign": [2, "except-parens"],
-    "space-infix-ops": 0,
     "no-unused-vars": [1, {"vars": "local", "args": "none"}],
-    "no-param-reassign": 0,
+    "object-shorthand": 1,
     "no-console": 2,
     "no-shadow": [1, {
       builtinGlobals: true,
       allow: ["resolve", "reject", "done", "cb", "status"],
     }],
     "template-curly-spacing": [1, "always"],
-    "react/jsx-key": 2,
+    "react/forbid-prop-types": 0,
     "react/no-direct-mutation-state": 2,
     "react/jsx-filename-extension": 0,
+    "react/react-in-jsx-scope": 0,
+    "react/prop-types": 1,
 
     // Class properties
     "class-property/class-property-semicolon": 1,
